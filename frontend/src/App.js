5a3776ad201a8b5e34c43943fb11262336029cc0
yenlingliu <nealliu1994@gmail.com>
2025-03-17 11:28:20 +1000

    update syntax for Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
