d4ef1aa90c4e4b32de9b6fee3f12e80d2de5a22a
yenlingliu <nealliu1994@gmail.com>
2025-03-20 10:33:16 +1000

    update baseUrl

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.106.126.4:5001', // live
+  baseURL: 'http://13.210.48.146:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
